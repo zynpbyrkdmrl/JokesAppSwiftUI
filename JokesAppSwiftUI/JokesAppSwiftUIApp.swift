593d1cc134baa0f54c212092ce163d1d794fbe33
Zeynep Bayrak Demirel <zeynepbayrak863@gmail.com>
2023-08-09 13:40:10 +0300

    Initial Commit

---
+++ JokesAppSwiftUI/JokesAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppSwiftUIApp.swift
+//  JokesAppSwiftUI
+//
+//  Created by Zeynep Bayrak Demirel on 9.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
